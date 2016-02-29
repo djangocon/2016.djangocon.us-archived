7be2e830c9d05ad5539a738908399b6b6bf26170
Jeff Triplett <jeff.triplett@gmail.com>
2016-02-29 14:09:26 -0600

    Lint'd urls

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -1,11 +1,10 @@
+import symposion.views
+
 from django.conf import settings
 from django.conf.urls import include, url
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
-
 from django.contrib import admin
-
-import symposion.views
+from django.views.generic import TemplateView
 
 
 urlpatterns = [
