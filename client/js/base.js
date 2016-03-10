3e2228bb02d3396794dbb79daad9cbb6134d5c87
Mark Wirblich <mark@wirblich.com>
2016-03-10 18:28:41 +0100

    Fixed JS bug

--- client/js/base.js
+++ client/js/base.js
@@ -4,23 +4,33 @@ var $ = require('jquery');
 global.jQuery = $;
 require('bootstrap');
 
-$(window).resize(function(e) {
-  var windowWidth = $(window).width();
-  if ( windowWidth > 1300 && windowWidth < 1600 ) {
-    $('.sheet').each(function(index,el){
-      var step = $(el).attr('data-degree') / 300;
-      var distance = windowWidth - 1300;
-      var rotation = step * distance;
-      $(el).css({
-        'transform': 'rotate(' + rotation  + 'deg)',
-        'webkitTransform': 'rotate(' + rotation  + 'deg)',
-        'mozTransform': 'rotate(' + rotation  + 'deg)',
-        'msTransform': 'rotate(' + rotation  + 'deg)',
-        'oTransform': 'rotate(' + rotation  + 'deg)'
+$(document).ready(function() {
+
+  function positionSheets(initial) {
+    var windowWidth = $(window).width();
+    if ( windowWidth > 1300 && windowWidth < 1600 || initial === true && windowWidth > 1300 ) {
+      $('.sheet').each(function(index,el){
+        var step = $(el).attr('data-degree') / 300;
+        var distance = windowWidth - 1300;
+        if (initial && windowWidth >= 1600) {
+          var distance = 300;
+        }
+        var rotation = step * distance;
+        $(el).css({
+          'transform': 'rotate(' + rotation  + 'deg)',
+          'webkitTransform': 'rotate(' + rotation  + 'deg)',
+          'mozTransform': 'rotate(' + rotation  + 'deg)',
+          'msTransform': 'rotate(' + rotation  + 'deg)',
+          'oTransform': 'rotate(' + rotation  + 'deg)'
+        });
       });
-    });
+    }
   }
-  // var theta = $(window).scrollTop() % Math.PI;
-  // $('#leftgear').css({ transform: 'rotate(' + theta + 'rad)' });
-  // $('#rightgear').css({ transform: 'rotate(-' + theta + 'rad)' });
+
+  $(window).resize(function(e) {
+    positionSheets();
+  });
+
+  positionSheets(true);
+
 });
