325d3cda5585387b59f910b171386e7e52068e11
Mark Wirblich <mark@wirblich.com>
2016-03-12 21:03:05 +0100

    Addef sheet animations

--- client/js/base.js
+++ client/js/base.js
@@ -6,7 +6,7 @@ require('bootstrap');
 
 $(document).ready(function() {
 
-  function positionSheets(initial) {
+  function rotateSheets(initial) {
     var windowWidth = $(window).width();
     if ( windowWidth > 1300 && windowWidth < 1600 || initial === true && windowWidth > 1300 ) {
       $('.sheet').each(function(index,el){
@@ -27,10 +27,104 @@ $(document).ready(function() {
     }
   }
 
+  function positionSheets(initial) {
+    if (initial) {
+
+      var intWidth = $(window).innerWidth();
+      var intHeight = $(window).innerHeight();
+
+      var x = intWidth / 2;
+      var y = intHeight / 2;
+
+      $('.sheet').each(function(index,el){
+        var orientation = $(el).attr('data-orientation');
+        if ( orientation === 'left' ) {
+          $(el).css({
+            'left': (x - $(el).width()/2) + 'px',
+            'top': 200 + 'px'
+          });
+        } else if ( orientation === 'right' ) {
+          $(el).css({
+            'right': (x - $(el).width()/2) + 'px',
+            'top': 200 + 'px'
+          });
+        }
+      });
+
+    } else {
+
+      var windowWidth = $(window).width();
+      var intWidth = $(window).innerWidth();
+      var intHeight = $(window).innerHeight();
+
+      var x = intWidth / 2;
+      var y = intHeight / 2;
+
+      if ( windowWidth > 1300 ) {
+
+        var distance = (windowWidth - 1300) / 2;
+
+        $('.sheet').each(function(index,el){
+          var destX = parseInt($(el).attr('data-posx'));
+          var orientation = $(el).attr('data-orientation');
+          console.log(orientation)
+          if ( orientation === 'left' ) {
+            if (windowWidth >= 1900) {
+              var posX = destX;
+            } else {
+              var elWidth = $(el).width()/2;
+              var path =  650 - elWidth - destX;
+              var stepX = path / 300;
+              var posX = 120 + path - stepX * distance;
+            }
+            $(el).css({
+              'left': posX + 'px',
+              'top': 200 + 'px'
+            });
+          } else if ( orientation === 'right' ) {
+            if (windowWidth >= 1900) {
+              var posX = destX;
+            } else {
+              var elWidth = $(el).width()/2;
+              var path =  650 - elWidth - destX;
+              var stepX = path / 300;
+              var posX = 120 + path - stepX * distance;
+            }
+            $(el).css({
+              'right': posX + 'px',
+              'top': 200 + 'px'
+            });
+          }
+
+        });
+
+      } else {
+
+        $('.sheet').each(function(index,el){
+          var orientation = $(el).attr('data-orientation');
+          if ( orientation === 'left' ) {
+            $(el).css({
+              'left': (x - $(el).width()/2) + 'px',
+              'top': 200 + 'px'
+            });
+          } else if ( orientation === 'right' ) {
+            $(el).css({
+              'right': (x - $(el).width()/2) + 'px',
+              'top': 200 + 'px'
+            });
+          }
+        });
+
+      }
+    }
+  }
+
   $(window).resize(function(e) {
+    rotateSheets();
     positionSheets();
   });
 
+  rotateSheets(true);
   positionSheets(true);
 
 });
