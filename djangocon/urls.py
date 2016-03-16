0d8d1387b9c0b9f154f05f07b222dd5a187be255
Jeff Triplett <jeff.triplett@gmail.com>
2016-03-15 22:30:52 -0500

    Adds Contact Us form + template

--- djangocon/urls.py
+++ djangocon/urls.py
@@ -9,23 +9,26 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r"^$", TemplateView.as_view(template_name="homepage.html"), name="home"),
-    url(r"^admin/", include(admin.site.urls)),
-
     url(r"^account/", include("account.urls")),
+    url(r"^admin/", include(admin.site.urls)),
+    url(r"^contact/", include("contact_form.urls")),
+]
 
-    url(r"^blog/", include("pinax.blog.urls")),
-
+# Symposion urls
+urlpatterns += [
     url(r"^dashboard/", symposion.views.dashboard, name="dashboard"),
-
-    url(r"^speaker/", include("symposion.speakers.urls")),
     url(r"^proposals/", include("symposion.proposals.urls")),
-    url(r"^sponsors/", include("symposion.sponsorship.urls")),
     url(r"^reviews/", include("symposion.reviews.urls")),
     url(r"^schedule/", include("symposion.schedule.urls")),
-
+    url(r"^speaker/", include("symposion.speakers.urls")),
+    url(r"^sponsors/", include("symposion.sponsorship.urls")),
     url(r"^teams/", include("symposion.teams.urls")),
+]
 
+# Pinax urls
+urlpatterns += [
     url(r"^boxes/", include("pinax.boxes.urls")),
+    url(r"^blog/", include("pinax.blog.urls")),
     url(r"^", include("pinax.pages.urls")),
 ]
 
