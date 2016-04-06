95bbb21fb360fe7537e5df295523b3574283b2c3
Bill Blanchard <bill@plumbdev.com>
2016-04-05 21:38:42 -0400

    Issue #88 Adds Speaker CSV download

--- djangocon/views.py
+++ djangocon/views.py
@@ -1,7 +1,28 @@
+import csv
+
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render
 
 from pinax.blog.models import Post
+from symposion.speakers.models import Speaker
 
 def homepage(request):
     return render(request, "homepage.html", {
         'latestposts': Post.objects.published().order_by("published")[:10]})
+
+@login_required
+def get_speakers_csv(request):
+    if request.user.is_superuser:
+        response = HttpResponse(content_type='text/csv')
+        response['Content-Disposition'] = 'attachment; filename="Djangocon Speakers.csv"'
+
+        writer = csv.writer(response)
+        writer.writerow(['Name', 'Email'])
+
+        for speaker in Speaker.objects.all():
+            writer.writerow([speaker.name, speaker.email])
+
+        return response
+
+    return HttpResponseForbidden("Forbidden")
