0a6bb25ea0f8000f94d9bd42495aa111da91d863
Jeff Triplett <jeff.triplett@gmail.com>
2016-04-29 11:27:48 -0500

    Updates talk / tutorial admins

--- djangocon/proposals/admin.py
+++ djangocon/proposals/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 
-from .models import TalkProposal
+from . import models
 
 
-admin.site.register(TalkProposal)
+class ProposalAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'speaker',
+        'speaker_email',
+        'kind',
+        'audience_level',
+        'cancelled',
+    ]
+
+    list_filter = [
+        'kind__name',
+        'result__accepted',
+    ]
+
+    raw_id_admin = [
+        'speaker',
+    ]
+
+
+@admin.register(models.TalkProposal)
+class TalkProposalAdmin(ProposalAdmin):
+    pass
+
+
+@admin.register(models.TutorialProposal)
+class TutorialProposalAdmin(ProposalAdmin):
+    pass
