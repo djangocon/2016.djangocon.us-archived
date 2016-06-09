d80b1f4ba709cc147ec52b4efe705d02efb2311f
Jeff Triplett <jeff.triplett@gmail.com>
2016-06-09 10:33:32 -0500

    Fixes sort_by_m2m to work with integer fields

--- djangocon/core/templatetags/core_tags.py
+++ djangocon/core/templatetags/core_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 def sort_by_m2m(iterable, m2m_key):
     m2m, key = m2m_key.split('.')
     for i in iterable:
-        keys = [getattr(related, key) for related in getattr(i, m2m).order_by(key)]
+        keys = [str(getattr(related, key)) for related in getattr(i, m2m).order_by(key)]
 
         i._key = ':'.join(keys)
     return sorted(iterable, key=lambda x: x._key)
